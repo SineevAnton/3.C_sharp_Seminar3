a03746205261696bdf4f5a9e6d4cbcf56a742f99
Anton Sineev <sineevanton@mail.ru>
2022-05-21 15:11:41 +0300

    Task27 complete

---
+++ Task27/Task27.cs
@@ -0,0 +1,18 @@
+﻿// Task27.
+// Write a programm which returns a sum of all digits of an input number
+
+Console.Write("Enter a number: ");
+int num = int.Parse(Console.ReadLine());
+
+int getSummaOfDigits(int number)
+{
+    int summa = 0;
+    while (number != 0)
+    {
+        summa += number % 10;
+        number /= 10;
+    }
+    return summa;
+}
+
+Console.WriteLine($"The sum {num} digits is {getSummaOfDigits(num)}");
