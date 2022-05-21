ecfb0eb885a04da4f1744b415d85539c5aa18827
Anton Sineev <sineevanton@mail.ru>
2022-05-21 15:43:39 +0300

    Task29 complete

---
+++ Task29/Task29.cs
@@ -0,0 +1,26 @@
+﻿// Task29.
+// Write a programm which generates an array with the length, defined by user,
+// and fill it with random integer numbers and write it to console
+
+Console.Write("Enter the array size: ");
+int arrSize = int.Parse(Console.ReadLine());
+
+int[] generateAndFillArray(int size)
+{
+    int[] arr = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = new Random().Next(0, 100); //We can ask user to choose limits for random, but as u said before: "don't complicate the solution"))
+    }
+    return arr;
+}
+
+void printArray(int[] arr)
+{
+    var st = string.Join(", ", arr);
+    Console.WriteLine("Your array is:");
+    Console.WriteLine("[{0}]", st);
+}
+
+int[] testArray = generateAndFillArray(arrSize);
+printArray(testArray);
